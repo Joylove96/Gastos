cd3b6770cde8e6997c0d851f705ad80c20c36b23
Enrique JoyLove <keke.lolx3@gmail.com>
2026-07-26 16:41:24 -0400

    Resize re-entry Lock screen logo to 120x120 per user reference

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    NUNCA cachea: Supabase, CDN externo, ni APIs.
    Network-first siempre para APIs + CDN. */
 
-const CACHE = "joyregist-v18";
+const CACHE = "joyregist-v19";
 
 const LOCAL_FILES = [
   "./",
